969d3233d8cb30eeba7628a5aedb19a16b4212fe
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-07-06 21:30:06 -0400

    completed form and components

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,9 +6,10 @@ import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import App from "./components/App";
 import smurfReducer from "./reducers/smurfReducer";
-const store = createStore(
-    smurfReducer, applyMiddleware(thunk)
-)
-ReactDOM.render(<Provider store={store}>
-                    <App />
-                </Provider>, document.getElementById("root"));
+const store = createStore(smurfReducer, applyMiddleware(thunk));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
